0380024506778a5c2292f0e00d0bb187831c65a9
jack <yangj26952@gmail.com>
2016-02-06 21:18:38 +0800

    Modify router visit

--- express1/blog/routes/index.js
+++ express1/blog/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
+//var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router = function(app){
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express123' });
+  });
+};
 module.exports = router;
