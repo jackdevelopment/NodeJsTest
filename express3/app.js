8a7eaf0ef4c99b5be32772f8350bc2a6aa8e3d03
jack <yangj26952@gmail.com>
2016-02-13 21:23:31 +0800

    Add upload file function

--- express3/app.js
+++ express3/app.js
@@ -1,6 +1,20 @@
 var express1 = require('express');
 var path = require('path');
+var multer = require('multer')
+
+var storage = multer.diskStorage({
+   destination: function(req, file, cb) {
+      cb(null, '/home/user1/uploads');
+   },
+   filename: function(req, file, cb) {
+      //console.log(file);
+      cb(null, file.originalname + '-' + Date.now());
+   }
+});
+
+var upload = multer({storage: storage});
 var app = express1();
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,4 +26,17 @@ app.listen(1337, function() {
 app.get("/index.js", function(req, res){
    res.render('index',{title: 'express'});
 
+});
+
+app.get("/upload", function(req, res){
+   res.render('upload',{
+      title: 'file upload'
+   });
+});
+
+var cpUpload = upload.fields([{name: 'file1', maxCount: 1},{name: 'file2', maxCount: 1}])
+
+app.post("/upload", cpUpload, function(req, res, next){
+  // console.log(req.file);
+   res.redirect('/upload');
 });
